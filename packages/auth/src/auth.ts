f4b2052a47f9abbbd28fb1868138ab1603fc1657
shakilahmedriyad <riyad.ahmed1886@gmail.com>
2026-09-19 05:38:39 +0600

    setup auth package and database package

---
+++ packages/auth/src/auth.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "@repo/database";
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL as string,
+});
+
+const prisma = new PrismaClient({ adapter });
+export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  trustedOrigins: ["http://localhost:3000"],
+});
